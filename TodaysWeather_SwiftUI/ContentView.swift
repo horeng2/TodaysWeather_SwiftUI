7d3774002f660354ec790ca470df0ace1f0eeb34
horeng2 <huski.a.a@gmail.com>
2022-08-27 15:09:57 +0900

    프로젝트 생성

---
+++ TodaysWeather_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodaysWeather_SwiftUI
+//
+//  Created by 서녕 on 2022/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodaysWeather_SwiftUI/TodaysWeather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaysWeather_SwiftUIApp.swift
+//  TodaysWeather_SwiftUI
+//
+//  Created by 서녕 on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct TodaysWeather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
