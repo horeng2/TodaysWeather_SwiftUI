14ffcdf764923a05d1a5fac9d8da84c502dde5d1
horeng2 <huski.a.a@gmail.com>
2022-09-12 18:15:28 +0900

    feat: CityListViewModel 구현

--- TodaysWeather_SwiftUI/Views/ListView/CityListViewModel.swift
+++ TodaysWeather_SwiftUI/Views/ListView/CityListViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+class CityListViewModel: ObservableObject {
+    let weatherUseCase: WeatherUseCase
+    var cancelBag = Set<AnyCancellable>()
+
+    @Published var weatherDataSource = [CityWeather]()
+    
+    init(weatherUseCase: WeatherUseCase) {
+        self.weatherUseCase = weatherUseCase
+    }
+    
+    func weather() {
+        self.weatherDataSource.removeAll()
+        City.allCases.forEach{ city in
+            weatherUseCase.fetchWeatherInfo(of: city)
+                .receive(on: DispatchQueue.main)
+                .sink { value in
+                    switch value {
+                    case .failure:
+                        break
+                    case .finished:
+                        break
+                    }
+                } receiveValue: { [weak self] weatherInfo in
+                    self?.weatherDataSource.append(weatherInfo)
+                }
+                .store(in: &cancelBag)
+        }
+    }
+}
