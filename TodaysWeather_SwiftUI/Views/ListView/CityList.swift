32c43c7558419687ca754a3fbc37f3f033bcb7b4
horeng2 <huski.a.a@gmail.com>
2022-09-15 15:53:55 +0900

    feat: PreogressView 구현

--- TodaysWeather_SwiftUI/Views/ListView/CityList.swift
+++ TodaysWeather_SwiftUI/Views/ListView/CityList.swift
@@ -9,32 +9,53 @@ import SwiftUI
 
 struct CityList: View {
     @Binding var dataSource: [CityWeather]
+    @State var isLoading = true
 
     var body: some View {
-            NavigationView {
-                ZStack {
-                    Image("sky")
-                        .resizable()
-                        .scaledToFill()
-                        .edgesIgnoringSafeArea(.all)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                        .opacity(1)
-                List {
-                    ForEach(dataSource, id: \.cityName) { cityWeather in
-                        NavigationLink {
-                            CityWeatherDetail(cityWeather: cityWeather)
-                        } label: {
-                            CityWeatherRow(cityWeather: cityWeather)
-                        }
-                    }
-                    .navigationTitle("오늘의 날씨".localize())
-                    .listRowSeparatorTint(Color.gray)
-                    .listRowBackground(Color.clear)
-                }
-                .listStyle(PlainListStyle())
-                .padding(.top, 90)
-            }
+        if isLoading {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .scaleEffect(2)
+                .task(loadingView)
+            
+        } else {
+            content
         }
+    }
+    
+    var content: some View {
+        NavigationView {
+            ZStack {
+                Image("sky")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .opacity(1)
+            List {
+                ForEach(dataSource, id: \.cityName) { cityWeather in
+                    NavigationLink {
+                        CityWeatherDetail(cityWeather: cityWeather)
+                    } label: {
+                        CityWeatherRow(cityWeather: cityWeather)
+                    }
+                }
+                .navigationTitle("오늘의 날씨".localize())
+                .listRowSeparatorTint(Color.gray)
+                .listRowBackground(Color.clear)
+            }
+            .listStyle(PlainListStyle())
+            .padding(.top, 90)
+        }
+    }
+    }
+}
+
+extension CityList {
+    @Sendable
+    func loadingView() async {
+        try? await Task.sleep(nanoseconds: 1_500_000_000)
+        self.isLoading = false
     }
 }
 
